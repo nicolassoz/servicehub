f576fb03183cc34aa20f82baea3c4896a2760dd9
Nicolas souza da silva <nnzllznn12@gmail.com>
2026-04-09 16:00:49 -0300

    mudou o monte de coisa

--- aulapdo.php
+++ aulapdo.php
@@ -1,4 +1,44 @@
 <?php
-
 include_once "config/conexao.php";
+
+$sql = "select * from servicos";
+$cmd = $pdo->prepare($sql);
+$cmd ->execute();
+
+$servicos = $cmd->fetchAll(PDO::FETCH_ASSOC);
+
+// var_dump($servicos);
 ?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>aula PDO php</title>
+</head>
+<body>
+    
+    <h2>lista de serviços</h2>
+    <table border="1" cellpadding = 10>
+        <tr>
+        <th>ID</th>
+        <th>Nome</th>
+        <th>Descrição</th>
+        <th>Preço</th>
+        <th>Descontinuado</th>
+        </tr>
+    <?php  foreach($servicos as $servico): ?>
+        <tr>
+        <td><?= $servico['id']?></td>
+        <!-- short echo -->
+        <td><?= $servico['nome']?></td>
+        <td><?= $servico['descricao']?></td>
+        <td><?= $servico['preco']?></td>
+        <td><?= $servico['descontinuado']?"Sim":"Não" ?></td>
+        </tr>
+    <?php endforeach; ?>
+    </table>
+
+</body>
+</html>
