01dff9e8fd5757bb403b5f0e6cac3bca8c2ba130
Nicolas souza da silva <nnzllznn12@gmail.com>
2026-04-09 14:32:30 -0300

    add e correção no conxao e add a aulapdo

--- config/conexao.php
+++ config/conexao.php
@@ -1,12 +1,13 @@
 <?php
 
-$host = "10.91.47.48";
-$db = "servicehubdb01";
+$host = "10.91.47.41";
+$db = "servicehubdb01";  // nome do banco de dados
 $user = "root";
 $pass = "P@ssw0rd";
-
+// php.net
 try{
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8",$user,$pass);
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 }catch(PDOException $e){
     die("Erro na conexão: ".$e->getMessage());
 }
