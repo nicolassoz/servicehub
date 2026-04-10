907c5acb071ec80368a2aac8d58d5fb478668a1a
Nicolas souza da silva <nnzllznn12@gmail.com>
2026-04-10 16:50:39 -0300

    criação do dormservico

---
+++ formservico.php
@@ -0,0 +1,51 @@
+<?php 
+
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
+
+ if($_SERVER['REQUEST_METHOD']=="POST")
+ {
+    require_once "config/conexao.php";
+    $nome = $_POST['txtnome'];
+    $descricao = $_POST['txtdescricao'];
+    $preco = $_POST['txtpreco'];
+
+    $sql = "insert servicos (nome, descricao, preco) values(:nome, :descricao, :preco)";
+    $cmd = $pdo->prepare($sql);
+    $cmd->execute([':nome'=>$nome, 'descricao'=>$descricao, 'preco'=>$preco]);
+
+    $id = $pdo->lastInsertId();
+
+    if(isset($id))
+        {
+            echo "serviço cadastrado com sucesso, com o ID".$id;
+        }
+    else
+        {
+            echo "falha ao cadastrado o serviço.";
+        }
+ }
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulário de cadastro de serviços</title>
+</head>
+<body>
+    <form  action="formservico.php" method="post">
+    <input type="number" name="txtid" id="" hidden>
+    <label for="txtnome">Nome</label>
+    <input type="text" name="txtnome" id="">
+    <label for="txtdescricao">Descrição</label>
+    <input type="text" name="txtdescricao" id="">
+    <label for="txtpreco">Preço</label>
+    <input type="text" name="txtpreco" id="">
+    <button type="submit">Gravar</button>
+    </form>
+</body>
+</html>
