3942f21fa565f240186a9925ffc5ecb6d262678c
Nicolas souza da silva <nnzllznn12@gmail.com>
2026-04-10 14:26:56 -0300

    agora ta certo

--- resform.php
+++ resform.php
@@ -2,12 +2,38 @@
 
 include_once "config/conexao.php";
 
-var_dump($_SERVER['REQUEST_METHOD']);
+if($_SERVER['REQUEST_METHOD']=="POST")
+    {
+        echo "<h3>chamado pela ação do formulario (POST) </h3>";
+
+        $id = $_POST['txtid'];
+        $sql = "select id, nome from servicos where id = :id";
+        $cmd = $pdo->prepare($sql);
+        $cmd->execute([':id'=>$id]);
+        $serve = $cmd->fetch(PDO::FETCH_ASSOC);
+        var_dump($serve);
+    }
+
+if($_SERVER['REQUEST_METHOD']=="GET")
+    {
+        echo "chamado pela url ou formulario method='get'";
+        
+        $idViaGet = $_GET['txtid'];
+        $sql = 'select * from servicos where id = :id';
+        $cmd = $pdo->prepare($sql);
+        $cmd->execute([':id'=>$idViaGet]);
+        $serviços = $cmd->fetchAll(PDO::FETCH_ASSOC);
+        var_dump($serviços);
+    }
+
+
+// var_dump($_SERVER['REQUEST_METHOD']);
 
 // $id = $_POST['txtid'];
 // $sql = "select id, nome from servicos";
 // $cmd = $pdo->prepare($sql);
 // $cmd->execute();
 // $serve = $cmd->fetch(PDO::FETCH_ASSOC);
+// var_dump($serve);
 
 // ?>
