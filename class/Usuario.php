fac5ff1139c2308672e540afcd99d204b948816e
Nicolas souza da silva <nnzllznn12@gmail.com>
2026-04-14 15:46:48 -0300

    getters e setters

--- class/Usuario.php
+++ class/Usuario.php
@@ -1,9 +1,83 @@
 <?php 
 // incluir a conexão
-
+include_once "config/conexao.php";
 // declara a classe
+class Usuario{
     // atributos
+    private $id;
+    private $nome;
+    private $email;
+    private $senha;
+    private $tipo;
+    private $ativo;
+    private $primeiro_login;
+    private $pdo;
 
     // construtor
+    public function __construct()
+    {
+        $this->pdo = obterPdo();
+    }
+    // getters / setters
+    public function getId()
+    {
+       return $this->id;
+    }
 
-    // métodos (functions)
+    public function getNome()
+    {
+        return $this->nome;
+    }
+    public function setNome(string $nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function getEmail()
+    {
+        return $this->nome;
+    }
+    public function setEmail(string $email)
+    {
+        $this->email = $email;
+    }
+
+    public function getSenha()
+    {
+        return $this->senha;
+    }
+    public function setSenha(string $senha)
+    {
+        $this->senha = $senha;
+    }
+
+    public function getTipo()
+    {
+        return $this->tipo;
+    }
+    public function setTipo(string $tipo)
+    {
+        $this->tipo = $tipo;
+    }
+
+    public function getAtivo()
+    {
+        return $this->ativo;
+    }
+    public function setAtivo(string $ativo)
+    {
+        $this->ativo = $ativo;
+    }
+
+    public function getPrimeiroLogin()
+    {
+        return $this->primeiro_login;
+    }
+    public function setPrimeiroLogin(string $primeiro_login)
+    {
+        $this->ativo = $primeiro_login;
+    }
+    // métodos (functions) - representam os RFs do projetos
+
+
+}
