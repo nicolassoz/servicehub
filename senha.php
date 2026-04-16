3b371b9040abea56efd57bb7f4caff5b5c692b49
Nicolas souza da silva <nnzllznn12@gmail.com>
2026-04-16 17:22:22 -0300

    inserindo usuario para teste

--- senha.php
+++ senha.php
@@ -1,4 +1,18 @@
 <?php 
-$senha = password_hash("123456",PASSWORD_DEFAULT);
-echo $senha;
+// $senha = password_hash("123456",PASSWORD_DEFAULT);
+// echo $senha;
+
+require_once "class/Usuario.php";
+
+$usuario = new Usuario();
+$usuario->setNome('Milharino Santos');
+$usuario->setEmail('Mil@harino.sa');
+$usuario->setSenha('mI2026@TV');
+$usuario->setTipo(2);
+
+if($usuario->inserir())
+    {
+        echo "usuário ".$usuario->getNome()." inserido com sucesso sob o ID".$usuario->getId();
+    }
+
 ?>
