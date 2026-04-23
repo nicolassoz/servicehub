db81526f95e223caa79ec45082094bb317c61cf2
Nicolas souza da silva <nnzllznn12@gmail.com>
2026-04-23 16:35:46 -0300

    atributo, construdor e getters / setters de servico

--- class/Servico.php
+++ class/Servico.php
@@ -1,7 +1,77 @@
 <?php
+// conexão
 include_once "config/conexao.php";
 
+// codigo para mostrar erro
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
 
+class servico
+{
+    // atributo
+    private $id;
+    private $nome;
+    private $descricao;
+    private $preco;
+    private $descontinuado;
+    private $pdo;
+
+    // construtor
+    public function __construct()
+    {
+        $this->pdo = obterPdo();
+    }
+
+    // getters / setters
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    private function setID(string $id)
+    {
+        $this->id = $id;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    private function setNome(string $nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function getDescricao()
+    {
+        return $this->descricao;
+    }
+
+    private function setDescricao(string $descricao)
+    {
+        $this->descricao = $descricao;
+    }
+
+    public function getPreco()
+    {
+        return $this->preco;
+    }
+
+    private function setPreco(string $preco)
+    {
+        $this->preco = $preco;
+    }
+
+    public function getDescontinuado()
+    {
+        return $this->descontinuado;
+    }
+
+    private function setDescontinuado(string $descontinuado)
+    {
+        $this->descontinuado = $descontinuado;
+    }
+}
