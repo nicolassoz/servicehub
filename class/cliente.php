5ca4ad00a47ce13ce9c5f67f056436f783d084ee
Nicolas souza da silva <nnzllznn12@gmail.com>
2026-04-23 14:28:28 -0300

    getters / setters

--- class/cliente.php
+++ class/cliente.php
@@ -16,4 +16,47 @@ class cliente
     {
         $this->pdo = obterPdo();
     }
+
+    // getters / setters
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    private function setID(string $id)
+    {
+        $this->id = $id;
+    }
+
+    public function getUsuarioId()
+    {
+        return $this->usuario_id;
+    }
+
+    private function setUsuarioId(string $usuario_id)
+    {
+        $this->usuario_id = $usuario_id;
+    }
+
+    public function getTelefone()
+    {
+        return $this->telefone;
+    }
+
+    private function setTelefone(string $telefone)
+    {
+        $this->telefone = $telefone;
+    }
+
+    public function getCpf()
+    {
+        return $this->cpf;
+    }
+
+    private function serCpf(string $cpf)
+    {
+        $this->cpf = $cpf;
+    }
+
 }
