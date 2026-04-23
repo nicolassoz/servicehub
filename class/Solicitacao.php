4088df45efb3a055dc6b41551ee9d06488a543e1
Nicolas souza da silva <nnzllznn12@gmail.com>
2026-04-23 17:13:42 -0300

    atributo de solicitação

---
+++ class/Solicitacao.php
@@ -0,0 +1,25 @@
+<?php
+// conexão
+include_once "config/conexao.php";
+
+// codigo para mostrar erro
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
+
+class servico
+{
+    // atributo
+    private $id;
+    private $cliente_id;
+    private $descricao_problema;
+    private $data_preferida;
+    private $status;
+    private $data_cad;
+    private $data_atualizacao;
+    private $data_resposta;
+    private $resposta_admin;
+    private $endereco;
+    
+    
+}
