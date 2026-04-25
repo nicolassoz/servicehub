7ba0d096f0c6e13d4ab85fbe3538da5214ac0670
Nicolas Souza da Silva <nnzllznn12@gmail.com>
2026-04-25 17:16:56 -0300

    getters de solicitação de serviço

--- class/ServicoSolicitacao.php
+++ class/ServicoSolicitacao.php
@@ -13,4 +13,42 @@ class ServicoSolicitacao
     private $servico_id;
     private $solicitacao_id;
     private $data_assoc;
+
+    // construtor
+    public function __construct()
+    {
+        $this->pdo = obterPdo();
+    }
+
+    // getters / setters
+
+    public function getServicoId()
+    {
+        return $this->servico_id;
+    }
+
+    private function setServicoId(string $servico_id)
+    {
+        $this->servico_id = $servico_id;
+    }
+
+    public function getSolicitacaoId()
+    {
+        return $this->solicitacao_id;
+    }
+
+    private function setSolicitacaoId(string $solicitacao_id)
+    {
+        $this->solicitacao_id = $solicitacao_id;
+    }
+
+    public function getDataAssoc()
+    {
+        return $this->data_assoc;
+    }
+
+    private function setDataAssoc(string $data_assoc)
+    {
+        $this->data_assoc = $data_assoc;
+    }
 }
